8ead9744c9833a0d98d6071bc3f23727bc7d4d5a
gayatriDev02 <blossomjasmine02@gmail.com>
2026-01-22 23:29:57 +0530

    first commit

--- Digital clock/app.js
+++ Digital clock/app.js
@@ -1,10 +1,9 @@
 const clock=document.querySelector(".clock");
-// const closck =document.getElementsByClassName("clock")  or
+
 
 
 //just like setTimeout
 setInterval(function(){
     let date=new Date();
-// console.log(date.toLocaleTimeString());
 clock.innerHTML=date.toLocaleTimeString();
 },1000)
